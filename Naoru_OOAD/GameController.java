30ae89020403a09baea3da7621771324b2c60bf1
Kelvin <jetixng@yahoo.com>
2020-08-20 01:20:58 +0800

    disabling board flipping

--- Naoru_OOAD/GameController.java
+++ Naoru_OOAD/GameController.java
@@ -218,7 +218,7 @@ public class GameController implements ActionListener {
 
         this.piecesSwitching();
         this.switchCurrentPlayer();
-        this.Flipboard();
+        //this.Flipboard();
         this.updateVisual();
         return true;
 
