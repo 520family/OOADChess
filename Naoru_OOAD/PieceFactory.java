296ecc9273edfcb71e45f0c373f80daa854026c8
zijianlow <47082082+zijianlow@users.noreply.github.com>
2020-08-19 19:05:54 +0800

    Update PieceFactory.java

--- Naoru_OOAD/PieceFactory.java
+++ Naoru_OOAD/PieceFactory.java
@@ -1,28 +1,23 @@
-import sun.java2d.SunCompositeContext;
-import sun.security.provider.Sun;
-
 public class PieceFactory {
     public Piece makePiece(String pieceName,boolean isBlue){
         switch (pieceName) {
             case "Sun":
                 return new Sun(isBlue);
-                break;
+                
             case "Plus":
                 return new Plus(isBlue);
-                break;
+               
             case "Triangle":
                 return new Triangle(isBlue);
-                break;
+               
             case "Chevron":
                 return new Chevron(isBlue);
-                break;
+                
             case "Arrow":
                 return new Arrow(isBlue);
-                break;
+                
             default:
-                return null;
-                break;
-        }
-            
+                return null;       
+        }      
     }
 }
