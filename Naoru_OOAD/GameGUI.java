12f90e74bf6906aebe64ff278d39b4b8fd6f7398
TTkchenGG <69840739+TTkchenGG@users.noreply.github.com>
2020-09-01 18:16:36 +0800

    Update GameGUI.java

--- Naoru_OOAD/GameGUI.java
+++ Naoru_OOAD/GameGUI.java
@@ -5,29 +5,31 @@ import java.util.Random;
 import javax.sound.sampled.*;
 import javax.swing.*;
 
-class GameGUI extends JFrame{
+class GameGUI extends JFrame implements ComponentListener{
 
     private JButton [][] buttons;
     private JButton start;
     private JButton save;
     private JButton load;
     private JLabel turn;
+    private int iconWidth, iconHeight;
 
 	public GameGUI(){
         super("Assignment");
-		JPanel p = new JPanel(new FlowLayout((FlowLayout.LEFT)));
+	JPanel p = new JPanel(new FlowLayout((FlowLayout.LEFT)));
         JPanel jp = new JPanel(new GridLayout(8,7));
-	    buttons = new JButton [8][7];
+	buttons = new JButton [8][7];
         start = new JButton("Start");
-		save = new JButton("Save");
+	save = new JButton("Save");
         load = new JButton("Load");
         turn = new JLabel();
-		p.add(start,  BorderLayout.WEST);
-		p.add(save, BorderLayout.WEST);
+	p.add(start,  BorderLayout.WEST);
+	p.add(save, BorderLayout.WEST);
         p.add(load, BorderLayout.WEST);
         p.add(turn, BorderLayout.WEST);
-		add(p, BorderLayout.NORTH);
-		add(jp, BorderLayout.CENTER);
+	add(p, BorderLayout.NORTH);
+	add(jp, BorderLayout.CENTER);
+	this.addComponentListener(this);
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 7; j++){
                 buttons[i][j] = new JButton();
@@ -39,6 +41,42 @@ class GameGUI extends JFrame{
         setVisible(true);
         setSize(500,500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+	
+    @Override
+    public void componentShown(ComponentEvent e) {
+            // ignore
+     }
+
+    @Override
+    public void componentResized(ComponentEvent e) {
+        Object source = e.getSource();
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 7; j++){
+            if (buttons[i][j].getIcon()!=null){
+                iconWidth = buttons[i][j].getWidth();
+                iconHeight = buttons[i][j].getHeight();
+                buttons[i][j].setIcon(resizeIcon(buttons[i][j].getIcon(), buttons[i][j].getWidth(), buttons[i][j].getHeight()));
+        }
+    }     
+    }
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+            // ignore
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+            // ignore
+    }
+   
+    public static Icon resizeIcon(Icon icon, int resizedWidth, int resizedHeight) {
+    ImageIcon imgIcon = (ImageIcon) icon;
+    Image img = imgIcon.getImage();  
+    Image resizedImage = img.getScaledInstance(resizedWidth, resizedHeight,  java.awt.Image.SCALE_SMOOTH);  
+    return new ImageIcon(resizedImage);
     }
 
     public JButton getStartButton(){
