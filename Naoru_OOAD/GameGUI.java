d9f55ce77c40470c9e468ca8e04a02aa85b991ef
Terence <tankahchee00@gmail.com>
2020-09-01 19:04:15 +0800

    Added instruction board

--- Naoru_OOAD/GameGUI.java
+++ Naoru_OOAD/GameGUI.java
@@ -15,33 +15,32 @@ class GameGUI extends JFrame implements ComponentListener{
     private int iconWidth, iconHeight;
 
 	public GameGUI(){
-        super("Assignment");
-	JPanel p = new JPanel(new FlowLayout((FlowLayout.LEFT)));
+        super("Webale Chess");
+    
+        JPanel p = new JPanel(new FlowLayout((FlowLayout.LEFT)));
         JPanel jp = new JPanel(new GridLayout(8,7));
-<<<<<<< Updated upstream
-	buttons = new JButton [8][7];
-=======
         JPanel instruction = new JPanel();
-	    buttons = new JButton [8][7];
->>>>>>> Stashed changes
+        buttons = new JButton [8][7];
         start = new JButton("Start");
-	save = new JButton("Save");
+        save = new JButton("Save");
         load = new JButton("Load");
         turn = new JLabel();
-	p.add(start,  BorderLayout.WEST);
-	p.add(save, BorderLayout.WEST);
+        p.add(start,  BorderLayout.WEST);
+        p.add(save, BorderLayout.WEST);
         p.add(load, BorderLayout.WEST);
         p.add(turn, BorderLayout.WEST);
-	add(p, BorderLayout.NORTH);
-	add(jp, BorderLayout.CENTER);
-	this.addComponentListener(this);
+        add(p, BorderLayout.NORTH);
+        add(jp, BorderLayout.CENTER);
+        this.addComponentListener(this);
+
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 7; j++){
-                buttons[i][j] = new JButton();
-                jp.add(buttons[i][j]);
-                buttons[i][j].setBackground(Color.WHITE);
-	        }
-	    }
+                    buttons[i][j] = new JButton();
+                    jp.add(buttons[i][j]);
+                    buttons[i][j].setBackground(Color.WHITE);
+            }
+        }
+
         instruction.add(InstructionBoard()); 
         JScrollPane temp = new JScrollPane(instruction);
         add(temp, BorderLayout.EAST);    
@@ -50,24 +49,24 @@ class GameGUI extends JFrame implements ComponentListener{
         setSize(1200,600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-	
+
     @Override
     public void componentShown(ComponentEvent e) {
             // ignore
-     }
+    }
 
     @Override
     public void componentResized(ComponentEvent e) {
         Object source = e.getSource();
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 7; j++){
-            if (buttons[i][j].getIcon()!=null){
-                iconWidth = buttons[i][j].getWidth();
-                iconHeight = buttons[i][j].getHeight();
-                buttons[i][j].setIcon(resizeIcon(buttons[i][j].getIcon(), buttons[i][j].getWidth(), buttons[i][j].getHeight()));
+                if (buttons[i][j].getIcon()!=null){
+                    iconWidth = buttons[i][j].getWidth();
+                    iconHeight = buttons[i][j].getHeight();
+                    buttons[i][j].setIcon(resizeIcon(buttons[i][j].getIcon(), buttons[i][j].getWidth(), buttons[i][j].getHeight()));
+                }
+            }     
         }
-    }     
-    }
     }
 
     @Override
@@ -111,17 +110,14 @@ class GameGUI extends JFrame implements ComponentListener{
         JOptionPane.showMessageDialog(null, "You can't move your enemy piece!");
     }
 
-<<<<<<< Updated upstream
     public void endGameMessage(String player){
         JOptionPane.showMessageDialog(null, "Player " + player + " win!");
-=======
+    }
+
     public JLabel InstructionBoard(){
         ImageIcon i = new ImageIcon("instruction.png");
         JLabel l = new JLabel(i);
 
         return l;
->>>>>>> Stashed changes
     }
 }
-
-
